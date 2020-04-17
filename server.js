0691c7833c38d6053295e33c62b61fc535574021
RaresSanduConstantin <59281487+RaresSanduConstantin@users.noreply.github.com>
2020-04-17 21:52:58 +0300

    add

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-octagonal-50188",
-    user: "postgres",
-    password: "raresrares12",
-    database: "smart-brain",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
